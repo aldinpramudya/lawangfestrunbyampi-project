efd06e1362038de91680e979f1f22062ed64c87b
aldinpramudya <aldinarielpramudya@gmail.com>
2025-10-10 05:38:05 +0700

    changes to presale 1

--- constants/index.ts
+++ constants/index.ts
@@ -4,14 +4,14 @@ export const linkRegistration = [
         name: "Early Bird",
         googleForm: "https://docs.google.com/forms/d/e/1FAIpQLSeFWEQPOxKgnblFt5kEJ8hV9lGCLQybLdb8DPfaeQjrNoLT3Q/viewform?usp=header",
         price: "135,000",
-        enabled: true,
+        enabled: false,
     },
     {
         id: 2,
         name: "Presale 1",
         googleForm: "https://docs.google.com/forms/d/e/1FAIpQLSf81IAuyCv1pv-VmECBdQTlffsKDSYYHR8GwQAJGMiIrA9dQg/viewform?usp=header",
         price: "155,000",
-        enabled: false,
+        enabled: true,
     },
     {
         id: 3,
